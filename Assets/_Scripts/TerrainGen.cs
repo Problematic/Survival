834b99516bb988f9e3d80c77033618799db35b94
Matthew Jewkes <mjewkes@gmail.com>
2012-07-17 01:20:57 -0300

    tweaks to world gen

--- Assets/_Scripts/TerrainGen.cs
+++ Assets/_Scripts/TerrainGen.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TerrainGen : MonoBehaviour{
 
 	public Terrain terrain;
-
+	public GameObject tree;
+	public List<GameObject> trees;
+	
 	[ContextMenu("Generate New Heightmap")]	
 	public void Gen() {
 		int w = terrain.terrainData.heightmapWidth, h = terrain.terrainData.heightmapHeight;
@@ -32,5 +35,27 @@ public class TerrainGen : MonoBehaviour{
 		}
 		terrain.terrainData.SetHeights(0, 0, map);
 		
+	}
+	[ContextMenu("Spawn Trees")]
+	public void SpawnStuff(){
+		var g = new GameObject();
+		for (int i =0;i<100;i++){
+			Vector3 rand = new Vector3(Random.Range(476f,670),7, Random.Range(876f,1070f));
+			if (Physics.CheckSphere(rand,5)){
+				continue;
+			}
+			Debug.Log (rand);
+			var go = (Instantiate (tree,rand-Vector3.up*2,Quaternion.identity) as GameObject);
+			trees.Add(go);
+			go.transform.parent=g.transform;
+		}
+	}
+	[ContextMenu("Destroy Trees")]
+	public void Destroytrees(){
+		for (int i=0; i<trees.Count;i++){
+			Destroy (trees[i]);
+			
+		}
+		trees.Clear();
 	}
 }
